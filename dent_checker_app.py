13a2ada121f65b49719bf4dac6c0f6f0a64616e7
ferastyb <ferastyb@yahoo.co.uk>
2026-01-11 22:03:10 +0000

    Update dent_checker_app.py

--- dent_checker_app.py
+++ dent_checker_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 from rules_engine import assess_damage
 
 
-APP_TITLE = "Fuselage Dent Checker (Prototype)"
+APP_TITLE = "Structural Damage Checker (Prototype)"
 DB_FILE = "assessments.db"
 RULES_DB = "rules.db"
 

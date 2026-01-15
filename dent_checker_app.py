abd215baae29eda3376f694c8656b2e78ee5afc7
ferastyb <ferastyb@yahoo.co.uk>
2026-01-15 11:28:38 +0000

    Update dent_checker_app.py

--- dent_checker_app.py
+++ dent_checker_app.py
@@ -865,7 +865,7 @@ with colB:
             elif not HAS_DAMAGE_MODELS:
                 dent_result = {"status": "skipped", "reason": "damage_models module not available"}
 
-                with st.expander("Dent model debug", expanded=False):
+            with st.expander("Dent model debug", expanded=False):
             st.json(dent_debug)
 
         # --------------
